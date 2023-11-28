fc38ee617c949df205246250e44faf84c8b2e631
Florent Poinsard <florent.poinsard@outlook.fr>
2023-11-27 18:42:11 -0600

    addition of the cli implementation of pre-release's milestone
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

---
+++ go/cmd/pre_release/milestone.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2023 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreedto in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pre_release
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"vitess.io/vitess-releaser/go/releaser/pre_release"
+)
+
+var createMilestone = &cobra.Command{
+	Use:   "create-milestone",
+	Short: "Create the next milestone for the future release.",
+	Run: func(cmd *cobra.Command, args []string) {
+		_, newMilestone := pre_release.NewMilestone()
+		out := newMilestone()
+		fmt.Println("New milestone created or found on GitHub:", out)
+	},
+}
