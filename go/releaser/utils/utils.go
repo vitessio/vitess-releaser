9b47994a6b59d2f6cc9804f12458b46324328446
Florent Poinsard <florent.poinsard@outlook.fr>
2024-02-15 13:11:45 -0600

    simplify code
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/releaser/utils/utils.go
+++ go/releaser/utils/utils.go
@@ -42,8 +42,8 @@ func Exec(cmd string, args ...string) string {
 func ExecWithError(cmd string, args ...string) (string, error) {
 	command := exec.Command(cmd, args...)
 	out, err := command.CombinedOutput()
-	if err == nil {
-		return string(out), nil
+	if err != nil {
+		return string(out), fmt.Errorf("%w: failed to execute: %s", err, command.String())
 	}
-	return string(out), fmt.Errorf("%w: failed to execute: %s", err, command.String())
+	return string(out), nil
 }
