a6bba978f88127523fe4fd0c65a0f6fd7251b477
Florent Poinsard <florent.poinsard@outlook.fr>
2024-06-04 16:38:15 -0600

    Better instructions for the release summary step
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/releaser/prerequisite/summary.go
+++ go/releaser/prerequisite/summary.go
@@ -25,6 +25,12 @@ import (
 func CheckSummary(state *releaser.State) []string {
 	return []string{
 		"If the release does not contain significant changes (i.e. a small patch release) then this step can be skipped",
+		"",
+		"We must verify that the summary file makes sense: no grammar/english mistake, the content is well organized, that the links are not broken.",
+		"We must treat this file as if it was a blog post, this is one of the main entrypoints for people wanting to learn more about the release.",
+		"",
+		"Over the release cycle, people add their bits to this file, but no one looks at the file as a whole, this is the time to do it.",
+		"",
 		fmt.Sprintf("The summary file is located in: ./changelog/%s.0/%s/summary.md.", state.VitessRelease.MajorRelease, releaser.RemoveRCFromReleaseTitle(state.VitessRelease.Release)),
 		"The summary file for a release candidate is the same as the one for the GA release.",
 	}
