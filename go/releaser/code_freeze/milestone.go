33de2cc0b8c9d28b4608f84b51794bfd7dd3d308
Florent Poinsard <florent.poinsard@outlook.fr>
2024-06-20 17:16:54 -0600

    Move PRs to newer milestone when doing RC-1
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/releaser/code_freeze/milestone.go
+++ go/releaser/code_freeze/milestone.go
@@ -29,6 +29,11 @@ func NewMilestone(state *releaser.State) (*logging.ProgressLogging, func() strin
 		TotalSteps: 5,
 	}
 
+	// Two extra steps if we are doing an RC-1 release
+	if state.Issue.RC == 1 {
+		pl.TotalSteps += 2
+	}
+
 	return pl, func() string {
 		var link string
 		defer func() {
@@ -60,6 +65,29 @@ func NewMilestone(state *releaser.State) (*logging.ProgressLogging, func() strin
 		pl.NewStepf("Creating Milestone %s on GitHub", newMilestone)
 		link = github.CreateNewMilestone(state.VitessRelease.Repo, newMilestone)
 		pl.NewStepf("New Milestone %s created: %s", newMilestone, link)
+
+		// Let's assume we release v20.0.0, the current milestone is v20.0.0, and new milestone which we
+		// just created is v21.0.0:
+		//
+		// During the RC-1, we want to move all opened PRs from the v20.0.0 milestone to the v21.0.0 milestone.
+		// All the PRs that are still opened on the v20.0.0 milestone are based on main, and thus need to be
+		// assigned the new milestone for main: v21.0.0. We might decide to backport an opened PR to the release
+		// branch, in which case GitHub Actions will assign the v20.0.0 milestone to the backport.
+		//
+		// This only applies to RC-1 releases. For patch releases, since the branch is frozen, the risk of
+		// merging a PR in a release that don't match the milestone is very slim.
+		if state.Issue.RC == 1 {
+			currentMilestone := fmt.Sprintf("v%s", releaser.RemoveRCFromReleaseTitle(state.VitessRelease.Release))
+			pl.NewStepf("Get opened Pull Requests for Milestone %s", currentMilestone)
+			prs := github.GetOpenedPRsByMilestone(state.VitessRelease.Repo, currentMilestone)
+
+			if len(prs) > 0 {
+				pl.NewStepf("Move %d Pull Requests to the %s Milestone", len(prs), newMilestone)
+				github.AssignMilestoneToPRs(state.VitessRelease.Repo, newMilestone, prs)
+			} else {
+				pl.NewStepf("No opened Pull Request found for Milestone %s, nothing to move", currentMilestone)
+			}
+		}
 		return link
 	}
 }
